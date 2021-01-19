92f639dcc881a1d66b69dbc85180004fb2389ab8
Cale Crawford <cale@typeninecreatives.com>
2021-01-18 17:03:03 -0800

    Backend

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  /* config options here */
-  images: {
-    domains: ['localhost'],
-  },
-}
--- types/author.ts
+++
@@ -1,7 +0,0 @@
-type Author = {
-  name: string
-  picture: string
-  image: string
-}
-
-export default Author
